f83e557cfed1e58bc831d3cb63bc6faf8cf76643
Ltype2018 <Ltype2018@outlook.com>
2020-08-19 23:31:56 +0800

    echarts

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <Vecharts :option="tdata" />
+  </div>
+</template>
+
+<script>
+import Vecharts from "@/components/Vecharts";
+export default {
+  name: "App",
+  components: {
+    Vecharts,
+  },
+  data() {
+    return {
+      tdata: {
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: "bar",
+            showBackground: true,
+            backgroundStyle: {
+              color: "rgba(220, 220, 220, 0.8)",
+            },
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Vecharts.vue
@@ -0,0 +1,34 @@
+<template v-if="option">
+    <div class="v-echarts" ref="charts">
+        echarts
+    </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+    name:"Vecharts",
+    props:{
+        option:{
+            type:Object,
+            required:true
+        }
+    },
+    mounted(){
+        this.initCharts(this.option)
+    },
+    methods:{
+        initCharts(){
+            let myChart = echarts.init(this.$refs.charts)
+            myChart.setOption(this.option)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.v-echarts{
+    width: 800px;
+    height: 600px;
+}
+</style>
